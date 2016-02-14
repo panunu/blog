c1641aa8dd3d35ca57f0b2ae9a07fe09468bfce9
Panu Leppäniemi <me@panuleppaniemi.com>
2016-02-14 20:23:38 +0200

    Styling

--- Blog.scala
+++ Blog.scala
@@ -4,22 +4,26 @@ object Blog extends App {
   val layout = readFile("layout.html")
   val posts = new File("posts").listFiles.filter(_.getName.endsWith(".html")).filter(_.getName != "index.html")
 
-  val links = posts.map((f: File) => {
+  val linksToPosts = posts.map((f: File) => {
     val content = readFile("posts/" + f.getName)
     val title = "<h2>(.*)</h2>".r.findFirstMatchIn(content).map(_.group(1)).getOrElse("Panun Blogsta")
+    val time = "<time>(.*)</time>".r.findFirstMatchIn(content).map(_.group(1)).getOrElse("")
     val html = layout
       .replaceAllLiterally("<title>Panun Blogsta</title>", s"<title>$title</title>")
       .replaceAllLiterally("<body></body>", s"<body>$content</body>")
 
     writeFile(f.getName, html)
 
-    (f.getName.substring(0, f.getName.length - 5), title)
-  })
+    (f.getName.substring(0, f.getName.length - 5), title, time)
+  }).reverse
 
-  val index = readFile("index.html").replaceAllLiterally("<li></li>", links.map { case (slug, title) => s"""<li><a href="/$slug">$title</a></li>""" }.mkString)
+  val index = readFile("index.html").replaceAllLiterally("<li></li>", linksToPosts.map {
+    case (slug, title, time) => s"""<li><time>$time</time><a href="/$slug">$title</a></li>"""
+  }.mkString)
+
   writeFile("index.html", layout.replaceAllLiterally("<body></body>", s"<body>$index</body>"))
 
-  println("\nBoom!\nBlog generated!\n")
+  println("\nBoom!\nBlog generated.\n")
 
   def readFile(file: String) = scala.io.Source.fromFile("./" + file).mkString
   def writeFile(file: String, content: String) = {
@@ -28,4 +32,5 @@ object Blog extends App {
     writer.close()
   }
 }
+
 
